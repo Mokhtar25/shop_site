0e2de8efec130f94833c9fc1c07039ac57380b22
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-19 20:47:38 +0200

    adding fav icon;

--- src/comp/Card.tsx
+++ src/comp/Card.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Product } from "../types";
+import Heart from "./../assets/heart.svg";
 
 interface Props {
   product: Product;
@@ -14,10 +15,14 @@ const Card = ({
   handelClickOpen,
   handelClickAddToCard,
 }: Props) => {
-  const [isfav, setFav] = useState(fav);
+  const [isFav, setFav] = useState(fav);
+
+  const handelFavClick = () => {
+    setFav(!isFav);
+  };
 
   return (
-    <div className="group flex h-72 w-52 cursor-pointer flex-col items-center justify-between rounded border-2 border-white p-4">
+    <div className="group relative flex h-72 w-52 cursor-pointer flex-col items-center justify-between rounded border-2 border-white p-4 before:absolute before:bottom-0 before:-z-30 before:h-[40%] before:w-full before:bg-pink-400 before:content-['']">
       <img
         src={product.images[0]}
         alt={"Picture of " + product.title}
@@ -28,8 +33,31 @@ const Card = ({
       <span className="mb-4 overflow-hidden duration-500 ease-in-out group-hover:mt-10 group-hover:transition-all">
         {product.price}€
       </span>
+      <HeartIcon
+        className="absolute bottom-2 right-2 size-6"
+        active={isFav}
+        onClick={handelFavClick}
+      />
     </div>
   );
 };
 
+interface IconProps {
+  className: string;
+  [key: string]: any; // Allow any additional props
+  active: boolean;
+  onClick: () => void;
+}
+
+const HeartIcon = ({ className, active, onClick, ...props }: IconProps) => (
+  <svg
+    onClick={onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={(active ? "fill-red-700 " : "fill-white ") + className}
+    {...props}
+  >
+    <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
+  </svg>
+);
 export default Card;
