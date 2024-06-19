567d352c2167b5c3494afdf0723ba4f7df37fa8b
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-19 17:21:50 +0200

    done with header

--- src/comp/Header.tsx
+++ src/comp/Header.tsx
@@ -1,37 +1,34 @@
 import { useState } from "react";
 
-// enum States { "home" | "about" | "shop"}
-
 export default function Header() {
   const [current, setCurrent] = useState("home");
 
-  const handelClick = (type: string) => {
-    console.log(type);
-    switch (type) {
-      case "home":
-        return setCurrent("home");
-      case "about":
-        return setCurrent("about");
-      case "shop":
-        setCurrent("shop");
-    }
+  const handelClick = (type: "home" | "about" | "shop") => {
+    setCurrent(type);
   };
 
+  const activeStyle = "drop-shadow-[0_1.2px_1.2px_rgba(168,85,247,.8)]";
   const stylelink =
-    "cursor-pointer select-none drop-shadow-md hover:text-slate-600 hover:shadow-black active:text-slate-900";
+    "cursor-pointer select-none drop-shadow-md hover:text-slate-600 hover:shadow-black active:text-slate-900 ";
   return (
     <nav className="flex h-16 bg-slate-100">
       <ul className="flex w-full items-center justify-center gap-8 text-xl text-slate-800">
         <li
-          className={stylelink + " border-2 border-fuchsia-700"}
+          className={stylelink + (current === "home" ? " " + activeStyle : "")}
           onClick={() => handelClick("home")}
         >
           Home
         </li>
-        <li className={stylelink} onClick={() => handelClick("about")}>
+        <li
+          className={stylelink + (current === "about" ? " " + activeStyle : "")}
+          onClick={() => handelClick("about")}
+        >
           About
         </li>
-        <li className={stylelink} onClick={() => handelClick("shop")}>
+        <li
+          className={stylelink + (current === "shop" ? " " + activeStyle : "")}
+          onClick={() => handelClick("shop")}
+        >
           Shop
         </li>
       </ul>
