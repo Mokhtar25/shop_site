0714180af52a55b3fe65a63e8537df3db501eeb0
Mokhtar <mokhtar2000naj@gmail.com>
2025-01-13 11:40:59 +0100

    build

--- src/comp/ShopPage.tsx
+++ src/comp/ShopPage.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, useContext, useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import Card from "./Card";
